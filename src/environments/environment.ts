8285885f36dc4e3a316601eb9ebcbc1f13f91426
Gabriele Iafigliola <gaboiaf@gmail.com>
2019-03-24 13:05:19 -0400

    firebase config sapbe2.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,21 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyCVnxyqMxuBrDZXpPTz9kqDQROP9IPWJs8",
-    authDomain: "sapbe-unimet.firebaseapp.com",
-    databaseURL: "https://sapbe-unimet.firebaseio.com",
-    projectId: "sapbe-unimet",
-    storageBucket: "sapbe-unimet.appspot.com",
-    messagingSenderId: "766167616658"
+  // firebase: {
+  //   apiKey: "AIzaSyCVnxyqMxuBrDZXpPTz9kqDQROP9IPWJs8",
+  //   authDomain: "sapbe-unimet.firebaseapp.com",
+  //   databaseURL: "https://sapbe-unimet.firebaseio.com",
+  //   projectId: "sapbe-unimet",
+  //   storageBucket: "sapbe-unimet.appspot.com",
+  //   messagingSenderId: "766167616658"
+  // },
+  firebase : {
+    apiKey: "AIzaSyBR51JpJScr2lUZhhMop2gpadfncDBtvdg",
+    authDomain: "sapbe2.firebaseapp.com",
+    databaseURL: "https://sapbe2.firebaseio.com",
+    projectId: "sapbe2",
+    storageBucket: "sapbe2.appspot.com",
+    messagingSenderId: "390045502917"
   }
 };
 
